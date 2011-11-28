5b5effc218e61098df56ffdf92c712900adf08d7
Simon Sarris <sarris@acm.org>
2011-11-28 00:20:17 -0500

    removed rotateDegrees to make the code smaller (and, well, there's no rotateDegrees in the context's methods)

--- transform.js
+++ transform.js
@@ -1,4 +1,5 @@
-// Last updated September 2011 by Simon Sarris
+// Last updated November 2011
+// By Simon Sarris
 // www.simonsarris.com
 // sarris@acm.org
 //
@@ -21,7 +22,7 @@
 // Remember that this does not account for any CSS transforms applied to the canvas
 
 function Transform() {
-  this.m = [1,0,0,1,0,0];
+  this.reset();
 }
 
 Transform.prototype.reset = function() {
@@ -63,20 +64,6 @@ Transform.prototype.invert = function() {
 };
 
 Transform.prototype.rotate = function(rad) {
-  var c = Math.cos(rad);
-  var s = Math.sin(rad);
-  var m11 = this.m[0] * c + this.m[2] * s;
-  var m12 = this.m[1] * c + this.m[3] * s;
-  var m21 = this.m[0] * -s + this.m[2] * c;
-  var m22 = this.m[1] * -s + this.m[3] * c;
-  this.m[0] = m11;
-  this.m[1] = m12;
-  this.m[2] = m21;
-  this.m[3] = m22;
-};
-
-Transform.prototype.rotateDegrees = function(angle) {
-  var rad = angle * Math.PI / 180;
   var c = Math.cos(rad);
   var s = Math.sin(rad);
   var m11 = this.m[0] * c + this.m[2] * s;
