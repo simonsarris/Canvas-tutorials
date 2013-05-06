c84129c9b6222fa278323bba604fbd228fa02f51
Bobby Jack <editor@fiveminuteargument.com>
2013-05-06 23:00:23 +0200

    Fixed typo in comment
    Width, not Height.

--- shapes.js
+++ shapes.js
@@ -29,7 +29,7 @@ Shape.prototype.draw = function(ctx) {
 // Determine if a point is inside the shape's bounds
 Shape.prototype.contains = function(mx, my) {
   // All we have to do is make sure the Mouse X,Y fall in the area between
-  // the shape's X and (X + Height) and its Y and (Y + Height)
+  // the shape's X and (X + Width) and its Y and (Y + Height)
   return  (this.x <= mx) && (this.x + this.w >= mx) &&
           (this.y <= my) && (this.y + this.h >= my);
 }
